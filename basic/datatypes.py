b11f8f36a44073d34eba9489234e03edb0216c15
VuyyuruSriBhavya08 <vuyyurusribhavya@gmail.com>
2026-06-18 22:05:36 +0530

    sample

---
+++ basic/datatypes.py
@@ -0,0 +1,24 @@
+# a=0B10
+# print(a,type(a))
+
+
+# a=10
+# print(bin(a),type(bin(a)),type)
+
+# print(oct(a),type(oct(a)))
+# print(hex(a),type(hex(a)))
+
+
+
+
+# a=True
+# b=False
+# dec=0b10
+# print(a+b)
+# print(a+dec)
+
+
+
+# lambda example
+big=lambda a,b: a if a>b else b
+print(big(2,3))
---
+++ programs/ways_to_print_LastIndex_value_in_string.py
@@ -0,0 +1,11 @@
+# for getting endting value of give string
+a="hey hie"
+print(a[len(a)-1])
+print(a[-1])
+
+
+
+# for getting starting value of give string
+a="hey hie"
+print(a[0])
+print(a[-len(a)])
